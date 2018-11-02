9fa3e221d41ec6aff0164e8fe0516f196955362b
argarak <dev@argarak.me>
2018-11-02 10:47:02 +0000

    Added style fixes and button events

--- web/js/main.js
+++ web/js/main.js
@@ -2,11 +2,42 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   console.log("DOM fully loaded and parsed");
 
-  var fileupload = document.getElementById("inputfile");
-  var openbtn = document.querySelector(".control-open");
+  window.quizObject = {
+    name: "",
+    author: "",
+    date: "",
+    questions: []
+  };
 
+  var fileupload = document.getElementById("inputfile");
+
+  fileupload.addEventListener('input', function (evt) {
+    console.log(this.files);
+
+    if (this.files[0].type !== "application/json" ||
+        this.files[0].type !== "text/plain") {
+      
+    }
+  });
+
+  var openbtn   = document.querySelector(".control-open");
+  var savebtn   = document.querySelector(".control-save");
+  var newbtn    = document.querySelector(".control-new");
+  var uploadbtn = document.querySelector(".control-upload");
+ 
   openbtn.addEventListener("click", function(e) {
     fileupload.click();
   });
 
+  savebtn.addEventListener("click", function(e) {
+    alert("save!");
+  });
+
+  newbtn.addEventListener("click", function(e) {
+    alert("new!");
+  });
+
+  uploadbtn.addEventListener("click", function(e) {
+    alert("upload!");
+  });
 });
---
+++ web/js/modal.js
@@ -0,0 +1,14 @@
+
+class Modal {
+  constructor() {}
+
+  error(msg) {
+    var modal = document.createElement("div");
+    modal.className += " errorModal modal";
+
+    var newContent = document.createTextNode("Hi there and greetings!"); 
+    newDiv.appendChild(newContent);  
+  }
+}
+
+var modal = new Modal();
