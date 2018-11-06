b712a8624d91e3eb6e2bac3d280db90ffa5e0ff0
argarak <dev@argarak.me>
2018-11-06 14:38:50 +0000

    Added display of questions and other fixes

--- web/js/create.js
+++ web/js/create.js
@@ -2,14 +2,61 @@ class Create {
   constructor() {}
 
   fromJSON(json) {
-    let template = `<div class="quizSection">
-                    <img class="quizIndicator" src="/icon/chevron-right.svg" alt="selector"/>
-                    <div class="quizTitle">
-                    ${title}
-                    </div>
-                    </div>`;
+    let title = "";
 
-    document.body.insertAdjacentHTML("beforeend", template);
+    if (!json.name) {
+      title = "New Quiz";
+    } else {
+      title = json.name;
+    }
+
+    let titleTemplate = `<div class="quizSection currentSection titleSection">
+
+                         <div class="indicatorContainer">
+                         <img class="quizIndicator" src="/icon/chevron-right.svg" alt="selector"/>
+                         </div>
+
+                         <div class="quizTitle">
+                         ${title}
+                         </div>
+
+                         </div>`;
+
+    let sideContainer = document.getElementById("sideContainer");
+
+    sideContainer.insertAdjacentHTML("beforeend", titleTemplate);
+
+    for (let question in json.questions) {
+      let questionNo = parseInt(question) + 1;
+      let id = "qu" + question;
+
+      let questionTemplate = `<div class="quizSection questionSection" id="${id}">
+
+                              <div class="indicatorContainer">
+                              <img class="quizIndicator" src="/icon/chevron-right.svg" alt="selector"/>
+                              </div>
+
+                              <div class="questionName">
+                              Question ${questionNo}
+                              </div>
+
+                              </div>`;
+
+      sideContainer.insertAdjacentHTML("beforeend", questionTemplate);
+
+      let questionElement = document.getElementById(id);
+
+      questionElement.addEventListener("click", function(e) {
+        let questionIndex = parseInt(e.target.id[e.target.id.length - 1]);
+        console.log(window.quizObject.questions[questionIndex].text);
+
+        document
+          .querySelector(".currentSection")
+          .classList.remove("currentSection");
+
+        e.target.className += " currentSection";
+      });
+    }
   }
 }
 
