97b0124a7e95c74b9c61f314a65640061b8f0da6
argarak <dev@argarak.me>
2018-11-06 14:59:00 +0000

    Added null checking for element selection

--- web/js/create.js
+++ web/js/create.js
@@ -1,6 +1,25 @@
 class Create {
   constructor() {}
 
+  /* Clears the side container of all elements */
+  clear() {}
+
+  editQuestion(qu, index) {
+    let content = document.getElementById("contentContainer");
+
+    let editTemplate = `
+
+    <div class="username">
+        <label for="uname">Question ${index}</label>
+        <input type="text" id="uname" name="uname" required
+               minlength="4" maxlength="8"
+               placeholder="4 to 8 characters long" />
+        <span class="validity"></span>
+    </div>`;
+
+    content.insertAdjacentHTML("beforeend", editTemplate);
+  }
+
   fromJSON(json) {
     let title = "";
 
@@ -26,6 +45,24 @@ class Create {
 
     sideContainer.insertAdjacentHTML("beforeend", titleTemplate);
 
+    let titleSection = document.querySelector(".titleSection");
+
+    if (titleSection === null) {
+      modal.error(
+        "Could not find class element titleSection. Please restart the program and try again."
+      );
+      this.clear();
+      return;
+    }
+
+    titleSection.addEventListener("click", function(e) {
+      document
+        .querySelector(".currentSection")
+        .classList.remove("currentSection");
+
+      e.target.className += " currentSection";
+    });
+
     for (let question in json.questions) {
       let questionNo = parseInt(question) + 1;
       let id = "qu" + question;
@@ -46,15 +83,37 @@ class Create {
 
       let questionElement = document.getElementById(id);
 
+      if (questionElement === null) {
+        modal.error(
+          "Could not find current questionElement ID. Please restart the program and try again."
+        );
+        this.clear();
+        return;
+      }
+
+      var self = this;
+
       questionElement.addEventListener("click", function(e) {
         let questionIndex = parseInt(e.target.id[e.target.id.length - 1]);
-        console.log(window.quizObject.questions[questionIndex].text);
 
-        document
-          .querySelector(".currentSection")
-          .classList.remove("currentSection");
+        var currentSection = document.querySelector(".currentSection");
+
+        if (currentSection === null) {
+          modal.error(
+            "Could not find class element currentSection. Please restart the program and try again."
+          );
+          this.clear();
+          return;
+        }
+
+        currentSection.classList.remove("currentSection");
 
         e.target.className += " currentSection";
+
+        self.editQuestion(
+          window.quizObject.questions[questionIndex],
+          questionIndex
+        );
       });
     }
   }
