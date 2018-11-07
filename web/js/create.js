67e79b6a82f1c10c6f1d46c047a563317e5d5940
Argarak <kzer-za@cryptolab.net>
2018-11-07 22:17:02 +0000

    Added edit choice inputs and styles

--- web/js/create.js
+++ web/js/create.js
@@ -1,23 +1,66 @@
 class Create {
   constructor() {}
 
+  /* Makes use of recursion */
+  findParentFromClass(el, className) {
+    if (el.parentNode === null || el.parentNode === undefined) {
+      return null;
+    }
+
+    if (
+      el.parentNode.classList === undefined ||
+      !el.parentNode.classList.contains(className)
+    ) {
+      this.findParentFromClass(el.parentNode, className);
+    }
+
+    return el.parentNode;
+  }
+
   /* Clears the side container of all elements */
-  clear() {}
+  clearSide() {
+    let sideContainer = document.getElementById("sideContainer");
+
+    if (sideContainer === null) {
+      console.error("Cannot clear side as #sideContainer cannot be located!");
+      return;
+    }
+
+    sideContainer.innerHTML = "";
+  }
 
   editQuestion(qu, index) {
     let content = document.getElementById("contentContainer");
+    content.innerHTML = "";
 
     let editTemplate = `
-
-    <div class="username">
-        <label for="uname">Question ${index}</label>
-        <input type="text" id="uname" name="uname" required
-               minlength="4" maxlength="8"
-               placeholder="4 to 8 characters long" />
+    <div class="editElement editQuestionText">
+        <label for="uname">Question Text</label>
+        <input type="text" id="uname" name="uname" required="" minlength="4" maxlength="8" placeholder="4 to 8 characters long">
         <span class="validity"></span>
-    </div>`;
+    </div>
+    `;
 
     content.insertAdjacentHTML("beforeend", editTemplate);
+
+    for (let i = 1; i < 5; i++) {
+      let choiceTemplate = `
+      <div class="editElement editChoice1" style="">
+          <label for="uname">Choice ${i}</label>
+          <input type="text" id="uname" name="uname" required="" minlength="4" maxlength="8" placeholder="4 to 8 characters long">
+          <span class="validity"></span>
+        
+          <div class="choiceExtendedOptions">
+              <div class="checkboxContainer">
+                  <label for="correct1">Correct</label>
+              	<input type="checkbox" name="correct${1}" value="Correct">
+              </div>
+          </div>
+      </div>   
+      `;
+
+      content.insertAdjacentHTML("beforeend", choiceTemplate);
+    }
   }
 
   fromJSON(json) {
@@ -43,6 +86,14 @@ class Create {
 
     let sideContainer = document.getElementById("sideContainer");
 
+    if (sideContainer === null) {
+      modal.error(
+        "Could not find ID element sideContainer. Please restart the program and try again."
+      );
+      this.clearSide();
+      return;
+    }
+
     sideContainer.insertAdjacentHTML("beforeend", titleTemplate);
 
     let titleSection = document.querySelector(".titleSection");
@@ -51,16 +102,19 @@ class Create {
       modal.error(
         "Could not find class element titleSection. Please restart the program and try again."
       );
-      this.clear();
+      this.clearSide();
       return;
     }
 
     titleSection.addEventListener("click", function(e) {
-      document
-        .querySelector(".currentSection")
-        .classList.remove("currentSection");
+      if (e.target.classList.contains("quizSection")) {
+        document
+          .querySelector(".currentSection")
+          .classList.remove("currentSection");
 
-      e.target.className += " currentSection";
+        e.target.className += " currentSection";
+      } else {
+      }
     });
 
     for (let question in json.questions) {
@@ -87,7 +141,7 @@ class Create {
         modal.error(
           "Could not find current questionElement ID. Please restart the program and try again."
         );
-        this.clear();
+        this.clearSide();
         return;
       }
 
@@ -102,7 +156,7 @@ class Create {
           modal.error(
             "Could not find class element currentSection. Please restart the program and try again."
           );
-          this.clear();
+          this.clearSide();
           return;
         }
 
